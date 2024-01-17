4702c33981a4d5507f3b9df70e8c9941d90699e7
Ryan Graham <ryanegraham1994@gmail.com>
2024-01-17 17:11:41 -0500

    rearrange + add new merge stuff

---
+++ src/Algorithms/Merge/OrderedStreamEnumerator.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+
+namespace Algorithms;
+
+public class OrderedStreamEnumerator<T>(IEnumerator<T> inner) : IEnumerator<T> where T : struct
+{
+    private readonly IEnumerator<T> _inner = inner;
+	public bool HasMore { get; set; }
+	public bool Initialized { get; set; }
+
+    object IEnumerator.Current => Current as object;
+
+	public T Current => _inner.Current;
+
+	public bool MoveNext()
+	{
+		if (!Initialized)
+			Initialized = true;
+		
+		HasMore = _inner.MoveNext();
+		
+		return HasMore;
+	}
+
+	public void Reset()
+	{
+		Initialized = false;
+		
+		_inner.Reset();
+		HasMore = false;
+	}
+
+	public void Dispose() => _inner.Dispose();
+}
